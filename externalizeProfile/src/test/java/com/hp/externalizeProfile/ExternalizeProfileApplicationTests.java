718a02048b4186eb1ceb9ade8214834312104a40
Chowdhury <deborshi.chowdhury@hp.com>
2022-11-30 02:21:46 +0530

    First commit

---
+++ externalizeProfile/src/test/java/com/hp/externalizeProfile/ExternalizeProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hp.externalizeProfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalizeProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
